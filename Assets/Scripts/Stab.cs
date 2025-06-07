76ee3d82581975910cedade7e29479365d4b0eb2
Parker Duren <park.duren180@gmail.com>
2025-06-07 09:52:44 -0500

    Stab init, area init

---
+++ Assets/Scripts/Stab.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stab : MonoBehaviour
+{
+    Vector3 knifeInitPos;
+    void Start()
+    {
+        knifeInitPos = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("s"))
+        {
+            Debug.Log("s");
+            // move knife to init pos
+            gameObject.transform.position = knifeInitPos;
+        }
+        else if (Input.GetKeyDown("w"))
+        {
+            Debug.Log("w");
+            // move knife to stabbed pos
+            gameObject.transform.position = new Vector3(knifeInitPos.x, knifeInitPos.y, knifeInitPos.z + 0.2f);
+        }
+    }
+}
