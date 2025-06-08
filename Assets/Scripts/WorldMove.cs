51d836081baf9a6b0c41385c96b1638319d8c233
Parker Duren <park.duren180@gmail.com>
2025-06-08 10:28:29 -0500

    death screen and red/yellow logic

--- Assets/Scripts/WorldMove.cs
+++ Assets/Scripts/WorldMove.cs
@@ -6,6 +6,14 @@ public class WorldMove : MonoBehaviour
 {
     [SerializeField] GameObject EntireWorld;
     Vector3 initialEntireWorldPos;
+    [SerializeField] List<GameObject> StopPoints;
+    int stopPointIndex = 0;
+    float distanceFromNextStopPoint;
+    [SerializeField] GameObject Player;
+    bool EXEC_redLightSwitch = false;
+    bool playerShouldCurrentlyBeStopped = false;
+    [SerializeField] GameObject DeathScreen;
+
     void Start()
     {
         initialEntireWorldPos = EntireWorld.transform.position;
@@ -14,6 +22,40 @@ public class WorldMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        EntireWorld.transform.position = Vector3.MoveTowards(EntireWorld.transform.position, new Vector3(0f, 0f, -1000f), 10f * Time.deltaTime);
+        if (Input.GetMouseButton(0))
+        {
+            EntireWorld.transform.position = Vector3.MoveTowards(EntireWorld.transform.position, new Vector3(0f, 0f, -1000f), 20f * Time.deltaTime);
+            if (playerShouldCurrentlyBeStopped)
+            {
+                Debug.Log("you have been t-boned hehe");
+                DeathScreen.SetActive(true);
+            }
+        }
+
+        distanceFromNextStopPoint = Vector3.Distance(Player.transform.position, StopPoints[stopPointIndex].transform.position);
+
+        if (distanceFromNextStopPoint < 30f)
+        {
+            EXEC_redLightSwitch = true;
+        }
+
+        if (distanceFromNextStopPoint < 60f)
+        {
+            Debug.Log("begin countdown to switch to green and begin stab portion");
+        }
+
+        if (EXEC_redLightSwitch)
+        {
+            Debug.Log("YEAH!!!!!!!!");
+
+            // red light
+            StopPoints[stopPointIndex].transform.GetChild(3).transform.GetChild(0).GetComponent<Light>().intensity = 3;
+
+            // yellow light
+            StopPoints[stopPointIndex].transform.GetChild(2).transform.GetChild(0).GetComponent<Light>().intensity = 0;
+
+            EXEC_redLightSwitch = false;
+            playerShouldCurrentlyBeStopped = true;
+        }
     }
 }
